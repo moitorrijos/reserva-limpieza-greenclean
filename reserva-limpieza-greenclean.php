cbe6bbfda0b64a251afea74511af482736bf7e65
moitorrijos <juanmtorrijos@gmail.com>
2020-11-06 02:39:39 -0500

    Version update.

--- reserva-limpieza-greenclean.php
+++ reserva-limpieza-greenclean.php
@@ -7,13 +7,13 @@
  * Author URI:      https://moitorrijos.com
  * Text Domain:     reserva-limpieza-greenclean
  * Domain Path:     /languages
- * Version:         3.16
+ * Version:         3.17
  *
  * @package         Reserva_Limpieza_Greenclean
  */
 
 defined( 'ABSPATH' ) || exit;
-define ( 'GCS_Version', '3.16' );
+define ( 'GCS_Version', '3.17' );
 
 include( plugin_dir_path( __FILE__ ) . 'includes/array_flatten.php');
 include( plugin_dir_path( __FILE__ ) . 'includes/get_gcs_holidays.php');
